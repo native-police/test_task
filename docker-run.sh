6cd9c49f64e2ef38d6c89e62d46e70f8bb9c95c7
native-police <53899536+native-police@users.noreply.github.com>
2021-11-07 00:55:00 +0500

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -2,23 +2,23 @@
 #Так как про использование unzip и git не было сказано, используем curl 
 
 #Скачиваем docker-compose.yml
-curl -O https://raw.githubusercontent.com/native-police/test_task/main/docker-compose.yml
+curl -O https://raw.githubusercontent.com/native-police/test_task/main/docker-compose.yml /
 #Скачиваем необходимые файлы для grafana
-mkdir grafana
-cd grafana
-curl -O https://raw.githubusercontent.com/native-police/test_task/main/grafana/grafana.ini
-mkdir dashboards 
-mkdir datasources 
-cd dashboards 
-curl -O https://raw.githubusercontent.com/native-police/test_task/main/grafana/dashboards/dashboard.json
-curl -O https://raw.githubusercontent.com/native-police/test_task/main/grafana/dashboards/dashboard.yml
-cd ~/grafana/datasources
-curl -O https://raw.githubusercontent.com/native-police/test_task/main/grafana/datasources/datasource.yml
+mkdir grafana /
+cd grafana /
+curl -O https://raw.githubusercontent.com/native-police/test_task/main/grafana/grafana.ini /
+mkdir dashboards  /
+mkdir datasources /
+cd dashboards /
+curl -O https://raw.githubusercontent.com/native-police/test_task/main/grafana/dashboards/dashboard.json /
+curl -O https://raw.githubusercontent.com/native-police/test_task/main/grafana/dashboards/dashboard.yml /
+cd ~/grafana/datasources /
+curl -O https://raw.githubusercontent.com/native-police/test_task/main/grafana/datasources/datasource.yml /
 
 #Скачиваем конфигурацию nginx
-cd 
-curl -O https://raw.githubusercontent.com/native-police/test_task/main/nginx.conf
+cd /
+curl -O https://raw.githubusercontent.com/native-police/test_task/main/nginx.conf /
 #Скачиваем конфиг prometheus 
-curl -O https://raw.githubusercontent.com/native-police/test_task/main/prometheus.yml
+curl -O https://raw.githubusercontent.com/native-police/test_task/main/prometheus.yml /
 #Поднимаем сервер
 sudo -s docker-compose up -d
